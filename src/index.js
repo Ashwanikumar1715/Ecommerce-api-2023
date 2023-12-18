bac359f7fec68b903288a24bb00139f1c7f076bc
ashwani kumar <105099645+Ashwanikumar1715@users.noreply.github.com>
2023-12-19 01:36:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://ashwanimartf.onrender.com/',
   credentials: true,
 }));
 
